cedb330ea541ebefa691c47e448b21eb87a9b759
junyamut <7576571+junyamut@users.noreply.github.com>
2025-07-01 16:26:31 +0800

    Methods renamed to be more clear

--- src/main/java/xyz/joseyamut/updatequerybuilder/repository/FoodCategoryRepository.java
+++ src/main/java/xyz/joseyamut/updatequerybuilder/repository/FoodCategoryRepository.java
@@ -6,21 +6,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import xyz.joseyamut.updatequerybuilder.domain.entity.FoodCategoryEntity;
-import xyz.joseyamut.updatequerybuilder.repository.interfaces.CustomRepository;
+import xyz.joseyamut.updatequerybuilder.repository.interfaces.CustomQueryRepository;
 
 @EnableJpaRepositories
-public interface FoodCategoryRepository extends JpaRepository<FoodCategoryEntity, Integer>, CustomRepository {
+public interface FoodCategoryRepository extends JpaRepository<FoodCategoryEntity, Integer>, CustomQueryRepository {
 
     @Query(
             value = "SELECT * FROM food_category ORDER BY food_category_id ASC",
             nativeQuery = true
     )
-    Page<FoodCategoryEntity> listAllById(Pageable pageable);
+    Page<FoodCategoryEntity> listByPageOrderById(Pageable pageable);
 
     @Query(
             value = "SELECT * FROM food_category ORDER BY name ASC",
             nativeQuery = true
     )
-    Page<FoodCategoryEntity> listAllByName(Pageable pageable);
+    Page<FoodCategoryEntity> listByPageOrderByName(Pageable pageable);
 
 }
