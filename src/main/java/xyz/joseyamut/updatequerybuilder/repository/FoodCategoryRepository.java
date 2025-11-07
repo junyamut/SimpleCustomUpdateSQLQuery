ebfe8e03be9d7bc1319ef0b27cdecfa562d596e5
junyamut <7576571+junyamut@users.noreply.github.com>
2025-11-07 16:41:00 +0800

    Woops! Annotation shouldn't be there;

--- src/main/java/xyz/joseyamut/updatequerybuilder/repository/FoodCategoryRepository.java
+++ src/main/java/xyz/joseyamut/updatequerybuilder/repository/FoodCategoryRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import xyz.joseyamut.updatequerybuilder.domain.entity.FoodCategoryEntity;
 
-@Repository
 public interface FoodCategoryRepository extends JpaRepository<FoodCategoryEntity, Integer>, ExtendedRepository {
 
     @Query(
